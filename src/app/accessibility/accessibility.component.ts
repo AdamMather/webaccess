96db02ea1b6c05edb4fa4e7cf502f3ed6637887f
Adam Mather <adam.mather@atos.net>
2018-03-12 11:43:18 +0000

    a2 app: remove nav buttons until fix can be sought

--- src/app/accessibility/accessibility.component.ts
+++ src/app/accessibility/accessibility.component.ts
@@ -14,7 +14,7 @@ import { Address } from './../core/address.model';
 })
 
 export class AccessibilityComponent implements OnInit {
-
+ 
   public record: Record;
   public records: Array<Record>;
 
@@ -22,21 +22,15 @@ export class AccessibilityComponent implements OnInit {
   public addresses: Array<Address>;
 
   public currentRecord: string;
-
-  public sector: { [key: string]: any } = {
-    value: null,
-    description: null
-  };
-
-  constructor(private router: Router,
-    private global: Global) { }
+  
+  constructor (private router: Router,
+		           private global: Global) { }
 
   ngOnInit() {
 
     this.global.path = this.router.url;
     this.record = new Record({});
     this.address = new Address({});
-
   }
 
   onSubmit(newForm: NgForm): void {
@@ -47,14 +41,21 @@ export class AccessibilityComponent implements OnInit {
     newForm.reset();
   }
 
-  getFocus(eleId: string): void {
-
+  getFocus(eleId: string): void {       
     document.getElementById(eleId).focus();
   }
 
-  onSelectionChange(entry): void {
-    // clone the object for immutability
-    this.sector = Object.assign({}, this.sector, entry);
-  }
+  onChangeTab(eleId: string): void {
 
+    let contactDetail = document.getElementById('contactDetail');
+    let contactAddress = document.getElementById('contactAddress');
+    let contactProfile = document.getElementById('contactProfile');
+    
+    contactDetail.classList.remove('active');
+    contactAddress.classList.remove('active');
+    contactProfile.classList.remove('active');
+
+    let ele = document.getElementById(eleId);
+    ele.classList.add('active');
+  }
 }
