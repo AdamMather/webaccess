0dd8f9f3ad68d89c17b3f44050ccfddbe310dec9
Adam Mather <adam.mather@atos.net>
2018-03-22 16:20:07 +0000

    a2 app: implement keyboard handler for error validation

--- src/app/accessibility/accessibility.component.ts
+++ src/app/accessibility/accessibility.component.ts
@@ -14,7 +14,7 @@ import { Address } from './../core/address.model';
 })
 
 export class AccessibilityComponent implements OnInit {
- 
+
   public record: Record;
   public records: Array<Record>;
 
@@ -22,15 +22,19 @@ export class AccessibilityComponent implements OnInit {
   public addresses: Array<Address>;
 
   public currentRecord: string;
-  
-  constructor (private router: Router,
-		           private global: Global) { }
+
+  public values: string;
+
+
+  constructor(private router: Router,
+    private global: Global) { }
 
   ngOnInit() {
 
     this.global.path = this.router.url;
     this.record = new Record({});
     this.address = new Address({});
+
   }
 
   onSubmit(newForm: NgForm): void {
@@ -41,8 +45,72 @@ export class AccessibilityComponent implements OnInit {
     newForm.reset();
   }
 
-  getFocus(eleId: string): void {       
+  getFocus(eleId: string): void {
     document.getElementById(eleId).focus();
+  }
+
+  onKey(this: any, event: any): void {
+
+    if (event.keyCode == 13) {
+      let attId = event.srcElement.getAttribute('id');
+
+      switch (attId) {
+        case 'errMsg_fnReq':
+        case 'errMsg_fnMinLgt':
+        case 'errMsg_fnMaxLgt': {
+          this.getFocus('firstName');
+          break;
+        }
+        case 'errMsg_lnReq':
+        case 'errMsg_lnMinLgt':
+        case 'errMsg_lnMaxLgt': {
+          this.getFocus('lastName');
+          break;
+        }
+        case 'errMsg_emReq':
+        case 'errMsg_emInv': {
+          this.getFocus('emailAddress');
+          break;
+        }
+        case 'errMsg_tlReq':
+        case 'errMsg_tlInv': {
+          this.getFocus('landline');
+          break;
+        }
+        case 'errMsg_mbReq':
+        case 'errMsg_mbInv': {
+          this.getFocus('mobile');
+          break;
+        }
+        case 'errMsg_a1Req': {
+          this.getFocus('addrLine1');
+          break;
+        }
+        case 'errMsg_a2Req': {
+          this.getFocus('addrLine2');
+          break;
+        }
+        case 'errMsg_ctReq': {
+          this.getFocus('city');
+          break;
+        }
+        case 'errMsg_cyReq': {
+          this.getFocus('county');
+          break;
+        }
+        case 'errMsg_pcReq':
+        case 'errMsg_pcInv': {
+          this.getFocus('postcode');
+          break;
+        }
+        case 'errMsg_wsReq':
+        case 'errMsg_wsInv': {
+          this.getFocus('website');
+          break;
+        }
+      }
+    }
+
   }
 
   onChangeTab(eleId: string): void {
@@ -50,7 +118,7 @@ export class AccessibilityComponent implements OnInit {
     let contactDetail = document.getElementById('contactDetail');
     let contactAddress = document.getElementById('contactAddress');
     let contactProfile = document.getElementById('contactProfile');
-    
+
     contactDetail.classList.remove('active');
     contactAddress.classList.remove('active');
     contactProfile.classList.remove('active');
