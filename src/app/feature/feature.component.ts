569f6060b466c84c0158044e4d3592a5c99ebf62
Adam Mather <adam.mather@atos.net>
2018-03-26 11:56:59 +0100

    a2 app: feature implementation of accessible navbar and multiple checkbox select all functionality

--- src/app/feature/feature.component.ts
+++ src/app/feature/feature.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+import { NgForm, FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 import { Router } from '@angular/router';
 import { Global } from './../core/global';
@@ -12,17 +13,30 @@ import { Global } from './../core/global';
 
 export class FeatureComponent implements OnInit {
 
+  vehicles: any;
+  isSelected = false;
+
+  matchAll: Promise<string>|null = null;
+
+  private resolve: Function|null = null;
+
   public canvas: HTMLCanvasElement;
+
 
   constructor(
     private router: Router,
-    private global: Global) { }
+    private global: Global,
+  ) {
+    this.reset();
+  }
+
+  reset() {
+    this.matchAll = new Promise<string>((resolve, reject) => { this.resolve = resolve; });
+  }
 
   ngOnInit() {
 
     this.global.path = this.router.url;
-
-    //let canvas = <HTMLCanvasElement>document.getElementById('goodCanvas1');
 
     let ctx = this.draw2dCanvas('goodCanvas1');
 
@@ -30,6 +44,21 @@ export class FeatureComponent implements OnInit {
     ctx = this.drawCircle(ctx);
     ctx = this.drawRectangle(ctx);
     ctx = this.drawTriangle(ctx);
+
+    //
+    this.setVehicles();
+  }
+
+  setVehicles() {
+    this.vehicles = [
+        {'type': 'car', 'make': 'Jaguar'},
+        {'type': 'car', 'make': 'Landrover'},
+        {'type': 'car', 'make': 'Mini'}
+    ]
+}
+
+  checkAll() {
+      this.resolve !(this.isSelected = !this.isSelected);
   }
 
   draw2dCanvas(eleId: string): CanvasRenderingContext2D {
@@ -38,7 +67,7 @@ export class FeatureComponent implements OnInit {
   }
 
   fillRectangle(context: CanvasRenderingContext2D): CanvasRenderingContext2D {
-    
+
     context.fillStyle = "red";
     context.fillRect(0, 0, 80, 40);
     context.strokeRect(0, 0, 80, 40);
@@ -47,7 +76,7 @@ export class FeatureComponent implements OnInit {
   }
 
   drawTriangle(context: CanvasRenderingContext2D): CanvasRenderingContext2D {
-    
+
     context.beginPath();
     context.moveTo(75, 50);
     context.lineTo(100, 75);
@@ -72,7 +101,7 @@ export class FeatureComponent implements OnInit {
     context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
     context.fillStyle = '#cccc00';
     context.fill();
-    
+
     context.lineWidth = 2;
     context.strokeStyle = '#000000';
     context.stroke();
@@ -81,7 +110,7 @@ export class FeatureComponent implements OnInit {
   }
 
   drawRectangle(context: CanvasRenderingContext2D): CanvasRenderingContext2D {
-    
+
     context.beginPath();
     context.moveTo(50, 200);
     context.lineTo(200, 200);
@@ -97,6 +126,5 @@ export class FeatureComponent implements OnInit {
 
     return context;
   }
-
 
 }
